2f1b83bb4a6d409dc12c93fa2818f52b4265d3c5
Ahmet Abdi <ahmetabdi@gmail.com>
2017-02-19 13:04:12 +0000

    Add realm

--- android/app/src/main/java/com/hackathonstarter/MainApplication.java
+++ android/app/src/main/java/com/hackathonstarter/MainApplication.java
@@ -4,6 +4,7 @@ import android.app.Application;
 import android.util.Log;
 
 import com.facebook.react.ReactApplication;
+import io.realm.react.RealmReactPackage;
 import com.rnfs.RNFSPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.facebook.react.ReactInstanceManager;
@@ -27,6 +28,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RealmReactPackage(),
             new RNFSPackage(),
             new VectorIconsPackage()
       );
